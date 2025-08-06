7a4784b434bc45ea2a22209c7b32f4063bda58a6
aisha-miranda <tashchanova@bk.ru>
2025-08-06 14:54:45 +0500

    Created class Main with method main() that has a cycle for infinite reading of command number input. Created printMenu() method, which prints the command menu.

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            printMenu();
+
+            int command = scanner.nextInt();
+            if (command == 1) {
+
+            } else if (command == 2) {
+
+            } else if (command == 3) {
+
+            } else if (command == 4) {
+                System.out.println("Пока!");
+                scanner.close();
+                return;
+            } else {
+                System.out.println("Такой команды нет");
+            }
+        }
+    }
+
+    static void printMenu() {
+        System.out.println("1 - Ввести количество шагов в день.");
+        System.out.println("2 - Изменить цель по количеству шагов за день.");
+        System.out.println("3 - Напечатать статистику за определенный месяц.");
+        System.out.println("4 - Выйти из приложения");
+    }
+}
