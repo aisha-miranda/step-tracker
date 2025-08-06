899b15e62cc13cd323ada81e6c9251b2c4bdc7e5
aisha-miranda <tashchanova@bk.ru>
2025-08-06 16:23:53 +0500

    Added method for saving the number of steps in the days array of a MonthData object.

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -11,4 +11,27 @@ public class StepTracker {
             monthToData[i] = new MonthData();
         }
     }
+
+    void addNewNumberStepsPerDay() {
+        System.out.println("Введите номер месяца");
+        int monthNumber = scanner.nextInt();
+        if ((monthNumber<1) || (monthNumber>12)){
+            System.out.println("Номер месяца введён неверно.");
+            return;
+        }
+        System.out.println("Введите день от 1 до 30 (включительно)");
+        int dayNumber = scanner.nextInt();
+        if ((dayNumber<1) || (dayNumber>30)){
+            System.out.println("Номер дня введён неверно.");
+            return;
+        }
+        System.out.println("Введите количество шагов");
+        int stepNumber = scanner.nextInt();
+        if ((stepNumber<0) || (stepNumber == 0)){
+            System.out.println("Количество шагов должно быть положительным числом.");
+            return;
+        }
+        MonthData monthData = monthToData[monthNumber-1];
+        monthData.days[dayNumber-1] = stepNumber;
+    }
 }
