9ecff14df7f99e751f52c160e8829845a06e682f
aisha-miranda <tashchanova@bk.ru>
2025-08-06 15:29:06 +0500

    Implemented methods that print the number of steps for each day of the month, return the overall amount of steps done in a month, and return the maximum amount of steps per day.

--- src/MonthData.java
+++ src/MonthData.java
@@ -1,3 +1,27 @@
 public class MonthData {
     int[] days = new int[30];
+
+    void printDayAndStepsFromMonth(){
+        for (int i = 0; i < days.length; i++) {
+            System.out.println((i+1) + "день " + days[i]);
+        }
+    }
+
+    int sumStepsFromMonth(){
+        int sumSteps = 0;
+        for (int i = 0; i<days.length; i++){
+            sumSteps+=days[i];
+        }
+        return sumSteps;
+    }
+
+    int maxSteps(){
+        int maxSteps = 0;
+        for (int i = 0; i< days.length; i++){
+            if (days[i]>maxSteps){
+                maxSteps = days[i];
+            }
+        }
+        return maxSteps;
+    }
 }
